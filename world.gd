18bf5528514ea722ce0f04f1eb847f63c8a84e23
Ramón Huidobro <hola@ramonh.dev>
2023-03-11 15:29:05 +0100

    Add basic networking

--- world.gd
+++ world.gd
@@ -1,7 +1,30 @@
 extends Node
 
+@onready var main_menu = $CanvasLayer/MainMenu
+@onready var address_entry = $CanvasLayer/MainMenu/MarginContainer/VBoxContainer/AddressEntry
+
+const Player = preload("res://player.tscn")
+const PORT = 9999
+var enet_peer = ENetMultiplayerPeer.new()
+
 
 # Called when the node enters the scene tree for the first time.
 func _unhandled_input(event):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
+
+
+func _on_host_button_pressed():
+	main_menu.hide()
+	enet_peer.create_server(PORT)
+	multiplayer.multiplayer_peer = enet_peer
+	
+	add_player(multiplayer.get_unique_id())
+
+func _on_join_button_pressed():
+	pass # Replace with function body.
+
+func add_player(peer_id):
+	var player = Player.instantiate()
+	player.name = str(peer_id)
+	add_child(player)
